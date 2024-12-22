d629dba095f687d382c0177edd260822977b631b
minjin Kim <minjintiger@gmail.com>
2024-12-21 21:48:27 -0500

    added main function

--- HillCipher.java
+++ HillCipher.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class HillCipher{
 
+    public static void main(String[] args){
+        Scanner inputScanner = new Scanner(System.in);
 
+        inputScanner.close();
+        return;
+    }
 }
